159aa2d0c7c654371c364f40bfc5857ba33b88ea
yennifer tobon <ymtobony@unal.edu.co>
2020-08-01 06:41:54 -0500

    main with all last two test cases

--- 0x00-lockboxes/main_0.py
+++ 0x00-lockboxes/main_0.py
@@ -10,3 +10,9 @@ print(canUnlockAll(boxes))
 
 boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
 print(canUnlockAll(boxes))
+
+boxes = [[1, 4, 5], [2], [5, 2], [3], [4, 1], [3, 5]]
+print(canUnlockAll(boxes))
+
+boxes = [[10, 3, 8, 9, 6, 5, 8, 1], [8, 5, 3, 7, 1, 8, 6], [5, 1, 9, 1], [], [6, 6, 9, 4, 3, 2, 3, 8, 5], [9, 4], [4, 2, 5, 1, 1, 6, 4, 5, 6], [9, 5, 8, 8], [6, 2, 8, 6]]
+print(canUnlockAll(boxes))
